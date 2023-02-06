a8e5b81d46e4c3da69e7477f557cea15a8b1bf3a
drumsko <drumsko.tommy@gmail.com>
2023-02-06 22:51:12 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace filenameget
+{
+
+    public class filenameget
+    {
+        static void Main()
+        {
+            string[] file_directory =  Directory.GetFiles(@"H:\趣味\ゲーム",".json");
+            int Count = file_directory.Count();
+
+            for(int i = 0; i < Count;i++)
+            {
+                file_directory[i] = Path.GetFileName(file_directory[i]);
+            }
+
+            foreach (string file in file_directory)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
